e9312bdd4f2b223e73c8b4badba8409a45b481cf
Tanu Upadhyay <96697977+tanucs@users.noreply.github.com>
2025-01-22 23:03:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+var hamburgerBtn = document.querySelector('.main-navbar .hamburger-btn');
+var navList = document.querySelector('.main-navbar .nav-list');
+var navListItems = document.querySelectorAll('.nav-list li a');
+
+hamburgerBtn.addEventListener('click', activeClass);
+
+function activeClass(){
+	hamburgerBtn.classList.toggle('active');
+	navList.classList.toggle('active');
+}
+
+for(var i = 0; i < navListItems.length; i++){
+	navListItems[i].addEventListener('click', listItemClicked);
+}
+
+function listItemClicked(){
+	hamburgerBtn.classList.remove('active');
+	navList.classList.remove('active');
+}
+
+
+var homeSection = document.querySelector('#home');
+window.addEventListener('scroll', pageScrollFunction);
+window.addEventListener('load', pageScrollFunction);
+
+function pageScrollFunction(){
+	if(window.scrollY > 150){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+
